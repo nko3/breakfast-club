f5bb2fe79edbfb96702377d8087e6035e041c2e3
Michael Hall <mike@michaelhall.ca>
2012-11-09 20:35:06 -0500

    Initial derby commit

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
-
-console.log('Server running at http://0.0.0.0:8000/');
+require('derby').run(__dirname + '/lib/server')
